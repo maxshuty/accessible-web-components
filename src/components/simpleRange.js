76c23ad5e9e84eb9ce6107884381fdd061159596
Max Poshusta <MaxPoshusta@gmail.com>
2022-01-02 21:31:48 -0600

    Adding simpleRange.js file

---
+++ src/components/simpleRange.js
@@ -0,0 +1,485 @@
+/*
+  SimpleRange v1.0.0 | Repo: https://github.com/maxshuty/accessible-web-components/src/components/simpleRange.js
+  By Max Poshusta | https://github.com/maxshuty | https://www.linkedin.com/in/maxposhusta/
+*/
+
+// Object containing common CSS styles so we can change them in once place
+const cssHelpers = Object.freeze({
+  sliderBackgroundColor: 'tomato',
+  sliderCircleSize: 20,
+  sliderCircleBackgroundColor: '#ffffff',
+  sliderCircleBorderColor: '#8b8b8b',
+  sliderCircleFocusColor: '#0074cc',
+});
+
+const constants = Object.freeze({
+  MIN: 'min',
+  MAX: 'max',
+  SLIDER_ID: 'minMaxSlider',
+  MIN_LABEL_ID: `minLabel`,
+  MAX_LABEL_ID: `maxLabel`,
+  RANGE_STOPPED_EVENTS: ['mouseup', 'touchend', 'keyup'],
+  CUSTOM_EVENT_TO_EMIT_NAME: 'range-changed',
+});
+
+const template = document.createElement('template');
+template.innerHTML = `
+      <style>
+        .min-max-slider { 
+          position: relative;
+          width: 100%;
+          text-align: center;
+        }
+           
+        .min-max-slider > label {
+          position: absolute;
+          left: -10000px;
+          top: auto;
+          width: 1px;
+          height: 1px;
+          overflow: hidden;
+        }
+          
+        .min-max-slider > .legend {
+          display: flex;
+          justify-content: space-between;
+        }
+          
+        .min-max-slider > .legend > * {
+          font-size: small;
+        }
+          
+        .min-max-slider > input {
+          --sliderColor: ${cssHelpers.sliderCircleBackgroundColor};
+          --sliderBorderColor: ${cssHelpers.sliderCircleBorderColor};
+          --sliderFocusBorderColor: ${cssHelpers.sliderCircleFocusColor};
+          cursor: pointer;
+          position: absolute;
+          -webkit-appearance: none;
+          outline: none !important;
+          background: transparent;
+          background-image: linear-gradient(to bottom, transparent 0%, transparent 30%, ${cssHelpers.sliderBackgroundColor} 30%, ${cssHelpers.sliderBackgroundColor} 60%, transparent 60%, transparent 100%);
+        }
+          
+        .min-max-slider > input::-webkit-slider-thumb {
+          -webkit-appearance: none;
+          appearance: none;
+          width: ${cssHelpers.sliderCircleSize}px;
+          height: ${cssHelpers.sliderCircleSize}px;
+          background-color: var(--sliderColor);
+          cursor: pointer;
+          border: 1px solid var(--sliderBorderColor);
+          border-radius: 100%;
+        }
+        
+        .min-max-slider > input::-moz-range-thumb {
+          width: ${cssHelpers.sliderCircleSize}px;
+          height: ${cssHelpers.sliderCircleSize}px;
+          background-color: var(--sliderColor);
+          cursor: pointer;
+          border: 1px solid var(--sliderBorderColor);
+          border-radius: 100%;
+        } 
+          
+        .min-max-slider > input::-webkit-slider-runnable-track,  
+        .min-max-slider > input::-moz-range-track {
+          cursor: pointer;
+        }
+          
+        .min-max-slider > input:focus::-webkit-slider-thumb {
+          /* Accessible border on focus */
+          border: 2px solid var(--sliderFocusBorderColor);
+        }
+
+        .min-max-slider > input:focus::-moz-range-thumb {
+            /* Accessible border on focus */
+            border: 2px solid var(--sliderFocusBorderColor);
+        }
+          
+        span.value {
+          height: 1.7em;
+          font-weight: bold;
+          display: inline-block;
+        }
+      
+        span.value.upper::before {
+          content: "- ";
+          white-space: pre;
+          display: inline-block;
+          margin-left: 0.4em;
+        }     
+      </style>
+      
+      <div id="${constants.SLIDER_ID}" class="min-max-slider">
+        <label id="${constants.MIN_LABEL_ID}" for="${constants.MIN}">Minimum</label> 
+        <input id="${constants.MIN}" name="${constants.MIN}" type="range" step="1" />
+        <label id="${constants.MAX_LABEL_ID}" for="${constants.MAX}">Maximum</label>
+        <input id="${constants.MAX}" name="${constants.MAX}" type="range" step="1" />
+      </div>
+  `;
+
+class SimpleRange extends HTMLElement {
+  constructor() {
+    super();
+
+    // Setting these variables so that we can clean up the event listeners
+    // on the disconnectedCallback, you can't just remove an anonymous function
+    // otherwise
+    this.emitRangeSelection = () => this.emitRange();
+    this.onRangeInput = (el) => this.update(el.target);
+
+    // Setting up the shadow DOM
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+
+  static get observedAttributes() {
+    return ['min-label', 'max-label', 'min-range', 'max-range'];
+  }
+
+  get sliderId() {
+    return this.getAttribute('id');
+  }
+
+  get minRange() {
+    return (
+      parseInt(this.getAttribute('min-range')) ||
+      parseInt(this.getAttribute('min')) ||
+      1
+    );
+  }
+  set minRange(minimumRange) {
+    this.setAttribute('min-range', minimumRange);
+    this.setAttribute('min', minimumRange);
+  }
+
+  get maxRange() {
+    return (
+      parseInt(this.getAttribute('max-range')) ||
+      parseInt(this.getAttribute('max')) ||
+      0
+    );
+  }
+  set maxRange(maximumRange) {
+    this.setAttribute('max-range', maximumRange);
+    this.setAttribute('max', maximumRange);
+  }
+
+  get numberOfLegendItemsToShow() {
+    // If the consumer has specified the number of items to show and the number is at least 2
+    // then we return that number, else we return 2 as there always needs to be at least 2
+    const numOfLegendItems = parseInt(
+      this.getAttribute('number-of-legend-items-to-show')
+    );
+    return numOfLegendItems && numOfLegendItems > 1 ? numOfLegendItems : 2;
+  }
+
+  get hideLegend() {
+    return this.hasAttribute('hide-legend');
+  }
+
+  get hideLabel() {
+    return this.hasAttribute('hide-label');
+  }
+
+  get sliderColor() {
+    return this.getAttribute('slider-color');
+  }
+
+  get circleColor() {
+    return this.getAttribute('circle-color');
+  }
+
+  get circleBorderColor() {
+    return this.getAttribute('circle-border-color');
+  }
+
+  get circleFocusBorderColor() {
+    return this.getAttribute('circle-focus-border-color');
+  }
+
+  get eventNameToEmitOnChange() {
+    return (
+      this.getAttribute('event-name-to-emit-on-change') ||
+      constants.CUSTOM_EVENT_TO_EMIT_NAME
+    );
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    switch (name) {
+      case 'min-label':
+        if (!newValue) {
+          return;
+        }
+
+        this.getEl(constants.MIN_LABEL_ID).innerText = newValue;
+        break;
+      case 'max-label':
+        if (!newValue) {
+          return;
+        }
+
+        this.getEl(constants.MAX_LABEL_ID).innerText = newValue;
+        break;
+    }
+  }
+
+  connectedCallback() {
+    this.init();
+  }
+
+  disconnectedCallback() {
+    // Removing event listeners
+    const slider = this.getEl(constants.SLIDER_ID);
+
+    const min = slider.querySelector(`#${constants.MIN}`);
+    this.removeEventListeners(
+      min,
+      constants.RANGE_STOPPED_EVENTS,
+      this.emitRangeSelection,
+      false
+    );
+    this.removeEventListeners(min, ['input'], this.onRangeInput, false);
+
+    const max = slider.querySelector(`#${constants.MAX}`);
+    this.removeEventListeners(
+      max,
+      constants.RANGE_STOPPED_EVENTS,
+      this.emitRangeSelection,
+      false
+    );
+    this.removeEventListeners(max, ['input'], this.onRangeInput, false);
+  }
+
+  dispatchCustomEvent(el, event) {
+    const objToDispatchFrom = el ? el : window;
+    objToDispatchFrom.dispatchEvent(event);
+  }
+
+  // Helper method to add event listeners for multiple events
+  addMultipleEventListeners(element, events, handler) {
+    events.forEach((e) => element.addEventListener(e, handler));
+  }
+
+  // Helper method to remove listeners for multiple events
+  removeEventListeners(element, events, callBack, useCapture) {
+    events.forEach((event) => {
+      element.removeEventListener(event, callBack, useCapture);
+    });
+  }
+
+  getEl(id) {
+    return this.shadowRoot.getElementById(id);
+  }
+
+  // Setting the initial state of the application
+  init() {
+    this.setupColors();
+
+    const slider = this.getEl(constants.SLIDER_ID);
+
+    const min = slider.querySelector(`#${constants.MIN}`);
+    const max = slider.querySelector(`#${constants.MAX}`);
+
+    /* set data-values */
+    min.setAttribute('data-value', this.minRange);
+    max.setAttribute('data-value', this.maxRange);
+
+    slider.setAttribute('data-range-width', slider.offsetWidth);
+
+    this.createLabels(slider, min);
+    this.createLegend(slider);
+
+    const averageOfMinAndMax = (this.minRange + this.maxRange) / 2;
+    this.draw(slider, averageOfMinAndMax);
+
+    // Adding update event that updates the range selector
+    min.addEventListener('input', this.onRangeInput);
+    max.addEventListener('input', this.onRangeInput);
+
+    // Adding events when the user stops selecting a range
+    // this is then used to emit the events to whoever is
+    // consuming this component. If you did it in the `update`
+    // function it would be called hundreds of times, this allows
+    // us to only call it when the input has stopped.
+    this.addMultipleEventListeners(
+      min,
+      constants.RANGE_STOPPED_EVENTS,
+      this.emitRangeSelection
+    );
+    this.addMultipleEventListeners(
+      max,
+      constants.RANGE_STOPPED_EVENTS,
+      this.emitRangeSelection
+    );
+  }
+
+  // Emits new custom event for min-range-changed or max-range-changed so
+  // that the consumer of this component can do whatever they need when
+  // the values are changed
+  emitRange() {
+    const slider = this.getEl(constants.SLIDER_ID);
+    const min = slider.querySelector(`#${constants.MIN}`);
+    const max = slider.querySelector(`#${constants.MAX}`);
+
+    this.dispatchCustomEvent(
+      slider,
+      new CustomEvent(this.eventNameToEmitOnChange, {
+        bubbles: true,
+        composed: true,
+        detail: {
+          sliderId: this.sliderId,
+          minRangeValue: Math.floor(min.value),
+          maxRangeValue: Math.floor(max.value),
+        },
+      })
+    );
+  }
+
+  draw(slider, splitValue) {
+    const min = slider.querySelector(`#${constants.MIN}`);
+    min.setAttribute(constants.MIN, this.minRange);
+    min.setAttribute(constants.MAX, splitValue);
+
+    const max = slider.querySelector(`#${constants.MAX}`);
+    max.setAttribute(constants.MIN, splitValue);
+    max.setAttribute(constants.MAX, this.maxRange);
+
+    const rangeWidth = parseInt(slider.getAttribute('data-range-width'));
+    const thumbSize = cssHelpers.sliderCircleSize;
+
+    min.style.width = `${parseInt(
+      thumbSize +
+        ((splitValue - this.minRange) / (this.maxRange - this.minRange)) *
+          (rangeWidth - 2 * thumbSize)
+    )}px`;
+    max.style.width = `${parseInt(
+      thumbSize +
+        ((this.maxRange - splitValue) / (this.maxRange - this.minRange)) *
+          (rangeWidth - 2 * thumbSize)
+    )}px`;
+    min.style.left = '0px';
+    max.style.left = `${parseInt(min.style.width)}px`;
+
+    const lower = slider.querySelector('.lower');
+
+    let sliderHeight = min.offsetHeight;
+    if (!this.hideLabel) {
+      min.style.top = `${lower.offsetHeight}px`;
+      max.style.top = `${lower.offsetHeight}px`;
+
+      sliderHeight += lower.offsetHeight;
+    }
+
+    if (!this.hideLegend) {
+      const legend = slider.querySelector('.legend');
+      legend.style.paddingTop = `${min.offsetHeight}px`;
+
+      sliderHeight += +legend.offsetHeight;
+    }
+
+    slider.style.height = `${sliderHeight}px`;
+
+    if (max.value > this.maxRange - 1) {
+      // Correcting if it's 1 off at the end
+      max.setAttribute('data-value', this.maxRange);
+    }
+
+    max.value = max.getAttribute('data-value');
+    min.value = min.getAttribute('data-value');
+
+    if (!this.hideLabel) {
+      const upper = slider.querySelector('.upper');
+
+      lower.innerHTML = min.getAttribute('data-value');
+      upper.innerHTML = max.getAttribute('data-value');
+    }
+  }
+
+  update(el) {
+    const slider = el.parentElement;
+    const min = slider.querySelector(`#${constants.MIN}`);
+    const max = slider.querySelector(`#${constants.MAX}`);
+    const minValue = Math.floor(min.value);
+    const maxValue = Math.floor(max.value);
+
+    // Setting the inactive values before draw
+    min.setAttribute('data-value', minValue);
+    max.setAttribute('data-value', maxValue);
+
+    const averageOfMinAndMax = (minValue + maxValue) / 2;
+    this.draw(slider, averageOfMinAndMax);
+  }
+
+  setupColors() {
+    const elements = this.shadowRoot.querySelectorAll(
+      '.min-max-slider > input'
+    );
+    elements.forEach((el) => {
+      if (this.sliderColor) {
+        el.style.backgroundImage = `linear-gradient(to bottom, transparent 0%, transparent 30%, ${this.sliderColor} 30%, ${this.sliderColor} 60%, transparent 60%, transparent 100%)`;
+      }
+
+      if (this.circleColor) {
+        el.style.setProperty('--sliderColor', this.circleColor);
+      }
+
+      if (this.circleBorderColor) {
+        el.style.setProperty('--sliderBorderColor', this.circleBorderColor);
+      }
+
+      if (this.circleFocusBorderColor) {
+        el.style.setProperty(
+          '--sliderFocusBorderColor',
+          this.circleFocusBorderColor
+        );
+      }
+    });
+  }
+
+  createLegend(slider) {
+    if (this.hideLegend) {
+      return;
+    }
+
+    // Sets the legend values (the numbers below the slider bar
+    // this is dynamic and can handle any number of items in the
+    // legend, that's why there is a loop in the event you have
+    // more than 2 values in the legend
+    let legend = document.createElement('div');
+    legend.classList.add('legend');
+    let legendvalues = [];
+
+    for (let i = 0; i < this.numberOfLegendItemsToShow; i++) {
+      legendvalues[i] = document.createElement('div');
+      const val = Math.round(
+        this.minRange +
+          (i / (this.numberOfLegendItemsToShow - 1)) *
+            (this.maxRange - this.minRange)
+      );
+      legendvalues[i].appendChild(document.createTextNode(val));
+      legend.appendChild(legendvalues[i]);
+    }
+
+    slider.appendChild(legend);
+  }
+
+  createLabels(slider, min) {
+    if (this.hideLabel) {
+      return;
+    }
+
+    let lower = document.createElement('span');
+    let upper = document.createElement('span');
+
+    lower.classList.add('lower', 'value');
+    upper.classList.add('upper', 'value');
+    lower.appendChild(document.createTextNode(this.minRange));
+    upper.appendChild(document.createTextNode(this.maxRange));
+
+    slider.insertBefore(lower, min.previousElementSibling);
+    slider.insertBefore(upper, min.previousElementSibling);
+  }
+}
+
+window.customElements.define('range-selector', SimpleRange);
