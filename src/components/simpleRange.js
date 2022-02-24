e7ac5f06205d934f8c56b583e8f456bca34a2910
Max Poshusta <MaxPoshusta@gmail.com>
2022-02-24 05:02:09 -0600

    Adding presetMax

--- src/components/simpleRange.js
+++ src/components/simpleRange.js
@@ -182,6 +182,7 @@ class SimpleRange extends HTMLElement {
   }
 
   get presetMax() {
+  }
   get numberOfLegendItemsToShow() {
     // If the consumer has specified the number of items to show and the number is at least 2
     // then we return that number, else we return 2 as there always needs to be at least 2
