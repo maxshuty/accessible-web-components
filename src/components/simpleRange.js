c77cc12ccb4c656747c3939022976e0a41f11bba
Max Poshusta <MaxPoshusta@gmail.com>
2022-04-05 05:56:44 -0500

    Adding helpful comment

--- src/components/simpleRange.js
+++ src/components/simpleRange.js
@@ -441,7 +441,7 @@ class SimpleRange extends HTMLElement {
     `;
   }
 
-  /
+  //
   // Emits new custom event for min-range-changed or max-range-changed so
   // that the consumer of this component can do whatever they need when
   // the values are changed
