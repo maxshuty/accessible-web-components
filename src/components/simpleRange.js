4515a182bc05c9c801f522ec2fafdb0d7dfe7d14
Max Poshusta <MaxPoshusta@gmail.com>
2022-06-21 05:43:32 -0500

    Setting up preset values

--- src/components/simpleRange.js
+++ src/components/simpleRange.js
@@ -451,6 +451,7 @@ class SimpleRange extends HTMLElement {
 
     const maxValue =
       this.presetMax && this.presetMax > this.presetMin
+        ? this.presetMax
   }
   // Emits new custom event for min-range-changed or max-range-changed so
   // that the consumer of this component can do whatever they need when
