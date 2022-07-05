cccaa5cd98435826c86e83fc1cc91408074a1682
Max Poshusta <MaxPoshusta@gmail.com>
2022-07-05 05:14:15 -0500

    Setting up reset functionality

--- src/components/simpleRange.js
+++ src/components/simpleRange.js
@@ -462,7 +462,7 @@ class SimpleRange extends HTMLElement {
   }
 
   setupResetFunctionality() {
-    window.addEventListener('r', (event) => {
+    window.addEventListener('ra', (event) => {
   // Emits new custom event for min-range-changed or max-range-changed so
   // that the consumer of this component can do whatever they need when
   // the values are changed
