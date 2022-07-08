11925f4f0e961faf0f4e203b694557c637acdcb6
Max Poshusta <MaxPoshusta@gmail.com>
2022-07-08 05:29:23 -0500

    Setting up reset functionality

--- src/components/simpleRange.js
+++ src/components/simpleRange.js
@@ -464,7 +464,7 @@ class SimpleRange extends HTMLElement {
   setupResetFunctionality() {
     window.addEventListener('range-reset', (event) => {
       if (
-        !event.detail
+        !event.detail 
         !event.detail.sliderId ||
       ) {
       
