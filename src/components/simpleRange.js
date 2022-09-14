6ad16caca172d05f1f791f30b6b16faa9dd3d408
Max Poshusta <MaxPoshusta@gmail.com>
2022-09-14 05:05:22 -0500

    Setting up slider offset

--- src/components/simpleRange.js
+++ src/components/simpleRange.js
@@ -535,7 +535,7 @@ class SimpleRange extends HTMLElement {
 
     if (!this.hideLegend) {
       const legend = slider.querySelector('.legend');
-      const offsetHeight = this.input
+      const offsetHeight = this.inputs
 
       sliderHeight += +legend.offsetHeight;
     }
