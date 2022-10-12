285c51970fcb136fefa40935430f4fde5d943cd4
Max Poshusta <MaxPoshusta@gmail.com>
2022-10-12 06:54:41 -0500

    Setting up the minValue

--- src/components/simpleRange.js
+++ src/components/simpleRange.js
@@ -580,8 +580,7 @@ class SimpleRange extends HTMLElement {
     let min = slider.querySelector(minQuerySelector);
     let max = slider.querySelector(maxQuerySelector);
 
-    let
-    
+    let minValue
 
     const averageOfMinAndMax = (minValue + maxValue) / 2;
     this.draw(slider, averageOfMinAndMax);
