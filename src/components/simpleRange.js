7c07fc71cdf522f8a4f546a4255f4eeb3118ca6d
Max Poshusta <MaxPoshusta@gmail.com>
2022-10-18 05:20:45 -0500

    Setting up inactive values before drawing

--- src/components/simpleRange.js
+++ src/components/simpleRange.js
@@ -587,7 +587,7 @@ class SimpleRange extends HTMLElement {
       return;
     }
 
-    let minSliderInput = slider.querySelector();
+    let minSliderInput = slider.querySelector(``);
     this.draw(slider, averageOfMinAndMax);
   }
 
