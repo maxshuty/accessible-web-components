934e32a39a4f79e077d015c0c5c66a1d1df06248
Max Poshusta <MaxPoshusta@gmail.com>
2022-11-10 05:31:49 -0600

    Adding constants

--- src/components/simpleRange.js
+++ src/components/simpleRange.js
@@ -694,7 +694,7 @@ class SimpleRange extends HTMLElement {
 
       upper.setAttribute('type', 'number');
       upper.setAttribute(constants.MIN, this.minRange);
-      upper.setAttribute(constants, this.maxRange);
+      upper.setAttribute(constants., this.maxRange);
 
       lower.addEventListener('input', this.onRangeInput);
       upper.addEventListener('input', this.onRangeInput);
