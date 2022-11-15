98b134eeaa2857d6f638c102ed32d8ddda2ca62b
Max Poshusta <MaxPoshusta@gmail.com>
2022-11-15 06:09:30 -0600

    Adding comment

--- src/components/simpleRange.js
+++ src/components/simpleRange.js
@@ -587,7 +587,7 @@ class SimpleRange extends HTMLElement {
       return;
     }
 
-    //
+    // Setting
     let minSliderInput = slider.querySelector(`#${constants.MIN}`);
     minSliderInput.setAttribute('data-value', minValue);
 
