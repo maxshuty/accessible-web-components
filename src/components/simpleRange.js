0b47476b832f3ac186095c8e7c518537225ac6a2
Max Poshusta <MaxPoshusta@gmail.com>
2022-11-20 05:58:07 -0600

    Adding comment

--- src/components/simpleRange.js
+++ src/components/simpleRange.js
@@ -600,7 +600,7 @@ class SimpleRange extends HTMLElement {
 
   isValidRangeSelection(el, minValue, maxValue) {
     // Checking if the values are within the acceptable range
-    
+    /
     if (this.inputsForLabels) {
       const isMinEl =
         el.hasAttribute(constants.RANGE_INPUT_DATA_LABEL_MIN) ||
