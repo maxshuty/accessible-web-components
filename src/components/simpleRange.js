493caae0e69c65fd5c6986106dd082f7de6c0ce2
Max Poshusta <MaxPoshusta@gmail.com>
2022-12-14 05:22:38 -0600

    Adding comment

--- src/components/simpleRange.js
+++ src/components/simpleRange.js
@@ -603,7 +603,7 @@ class SimpleRange extends HTMLElement {
     // since the min value should never be more than the maxvalue
     // and the maxValue never less than the minValue.
     // This could occur if the user has inputs enabled and manually
-    // types a wrong number into the input.
+    // types a wrong number into the input. 
     if (this.inputsForLabels) {
       const isMinEl =
         el.hasAttribute(constants.RANGE_INPUT_DATA_LABEL_MIN) ||
