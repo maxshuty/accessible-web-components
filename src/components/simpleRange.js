bf041335b7ecdc637d65c299267b6288ee71674b
Max Poshusta <MaxPoshusta@gmail.com>
2023-01-08 07:21:47 -0600

    Adding code comment

--- src/components/simpleRange.js
+++ src/components/simpleRange.js
@@ -499,7 +499,7 @@ class SimpleRange extends HTMLElement {
     max.value = minValue;
   }
 
-  // Adding event 
+  // Adding event l
   setupResetFunctionality() {
     window.addEventListener('range-reset', (event) => {
       if (
