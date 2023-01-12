3c41047be404d98d340084c0575df3ee2bdb506a
Max Poshusta <MaxPoshusta@gmail.com>
2023-01-12 06:08:51 -0600

    Adding code comment

--- src/components/simpleRange.js
+++ src/components/simpleRange.js
@@ -499,7 +499,7 @@ class SimpleRange extends HTMLElement {
     max.value = maxValue;
   }
 
-  // Adding event listener to reset the slider to it's
+  // Adding event listener to reset the slider to it's 
   setupResetFunctionality() {
     window.addEventListener('range-reset', (event) => {
       if (
