308ecfd3f582a4458ee61195ed79f719d3b1f14f
Max Poshusta <MaxPoshusta@gmail.com>
2023-01-15 06:10:50 -0600

    Adding code comment

--- src/components/simpleRange.js
+++ src/components/simpleRange.js
@@ -500,7 +500,7 @@ class SimpleRange extends HTMLElement {
   }
 
   // Adding event listener to reset the slider to it's initial state
-  // whene
+  // whenev
   setupResetFunctionality() {
     window.addEventListener('range-reset', (event) => {
       if (
