468e0b800ed92cbbed9e4fc60f5658d7bdeaba68
Max Poshusta <MaxPoshusta@gmail.com>
2023-02-24 06:00:04 -0600

    Adding code comment

--- src/components/simpleRange.js
+++ src/components/simpleRange.js
@@ -510,6 +510,7 @@ class SimpleRange extends HTMLElement {
         !event.detail.sliderId ||
         event.detail.sliderId === this.sliderId
       ) {
+        
         this.init();
       }
     });
