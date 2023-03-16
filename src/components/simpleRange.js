6933fa022c79a0858898f8d188ab48dad0a3f026
Max Poshusta <MaxPoshusta@gmail.com>
2023-03-16 05:28:52 -0500

    Adding code comment

--- src/components/simpleRange.js
+++ src/components/simpleRange.js
@@ -671,7 +671,7 @@ class SimpleRange extends HTMLElement {
       }
     }
 
-    // They are using the non-input version, the only way to
+    // They are using the non-input version, the only way to 
     return minValue !== maxValue;
   }
 
