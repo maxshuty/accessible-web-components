64dc933738927b99418775c115519211a95ef564
Max Poshusta <MaxPoshusta@gmail.com>
2023-04-08 07:18:58 -0500

    Adding code comment

--- src/components/simpleRange.js
+++ src/components/simpleRange.js
@@ -220,7 +220,7 @@ class SimpleRange extends HTMLElement {
   }
 
   get circleBorderColor() {
-    // Altering the 
+    // Altering the circle
     return this.getAttribute('circle-border-color');
   }
 
